e8b14547bfc1c49b0408ebc1c35ee91f8035fabf
Lucas Campos <skullcan@msn.com>
2019-12-30 20:55:06 -0300

    model criado

---
+++ database/migrations/1577749468383_clientes_schema.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ClientesSchema extends Schema {
+  up () {
+    this.create('clientes', (table) => {
+      table.increments()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('clientes')
+  }
+}
+
+module.exports = ClientesSchema
